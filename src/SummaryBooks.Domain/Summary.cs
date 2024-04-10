999fa045a21a71eaf8ca411543082a9b35b74f91
Jose Eduardo Del Orbe Minaya <jedelorbeminaya@outlook.com>
2024-04-10 18:31:40 -0400

    Init
    This's the initialization of developmet to project

---
+++ src/SummaryBooks.Domain/Summary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummaryBooks.Domain
+{
+    public class Summary
+    {
+        public int id { get; set; }
+        public String title { get; set; }
+        public string description { get; set; }
+    }
+}
---
+++ src/SummaryBooks.Persistence/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SummaryBooks.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummaryBooks.Persistence
+{
+    public class DataContext : DbContext 
+    {
+        public DataContext(DbContextOptions<DataContext> options) 
+        : base(options) 
+        {
+        }
+       
+        public DbSet<Summary> Summaries { get; set; }
+    }
+}
